0dfcc509718675efcc1998288e9d154e439d0ad4
prz23 <panruizheng@zju.edu.cn>
2021-07-30 11:00:04 +0800

    support sgx 0.3.5

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,15 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
+#![no_std]
+#![cfg_attr(all(feature = "mesalock_sgx", not(target_env = "sgx")), no_std)]
+#![cfg_attr(all(target_env = "sgx", target_vendor = "mesalock"), feature(rustc_private))]
+
+#[cfg(all(feature = "mesalock_sgx", not(target_env = "sgx")))]
+#[macro_use]
+extern crate sgx_tstd as std;
+use std::prelude::v1::*;
+
 #[macro_use]
 mod field;
 #[macro_use]
